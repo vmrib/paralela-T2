f2a1fdfb6fdd9df5f21be161382f3e64c1a193bb
Vinicius M Ribeiro <vmr20@inf.ufpr.br>
2023-06-15 14:59:53 -0300

    df

--- myBcast-slurm.sh
+++ myBcast-slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mpirun -N 1 myBroadcast 8000 4096
-mpirun -N 1 myBroadcast 8000 16384 
+mpirun -N 1 ./mpirun 8000 4096 1
+# mpirun -N 1 ./mpirun 8000 16384 1
 
 # você roda assim:
 #   sbatch --exclusive -N 8 myBcast-slurm.sh
