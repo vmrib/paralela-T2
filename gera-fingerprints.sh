c695493e4b35da800931ef451ba0e1b5ea4add1f
Vinicius M Ribeiro <vmr20@inf.ufpr.br>
2023-06-15 15:12:56 -0300

    sdfdf

--- gera-fingerprints.sh
+++ gera-fingerprints.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-ssh -t wz00 "echo yes" |    ssh w01 hostname
-ssh -t wz00 "echo yes" |    ssh w02 hostname
-ssh -t wz00 "echo yes" |    ssh w03 hostname
-ssh -t wz00 "echo yes" |    ssh w04 hostname
-ssh -t wz00 "echo yes" |    ssh w05 hostname
-ssh -t wz00 "echo yes" |    ssh w06 hostname
-ssh -t wz00 "echo yes" |    ssh w07 hostname
-ssh -t wz00 "echo yes" |    ssh w08 hostname
+ssh -t wz00 "echo yes" &&    ssh w01 hostname
+ssh -t wz00 "echo yes" &&    ssh w02 hostname
+ssh -t wz00 "echo yes" &&    ssh w03 hostname
+ssh -t wz00 "echo yes" &&    ssh w04 hostname
+ssh -t wz00 "echo yes" &&    ssh w05 hostname
+ssh -t wz00 "echo yes" &&    ssh w06 hostname
+ssh -t wz00 "echo yes" &&    ssh w07 hostname
+ssh -t wz00 "echo yes" &&    ssh w08 hostname
 
-ssh -t wz00 "echo yes" |    ssh wz01 hostname
-ssh -t wz00 "echo yes" |    ssh wz02 hostname
-ssh -t wz00 "echo yes" |    ssh wz03 hostname
-ssh -t wz00 "echo yes" |    ssh wz04 hostname
-ssh -t wz00 "echo yes" |    ssh wz05 hostname
-ssh -t wz00 "echo yes" |    ssh wz06 hostname
-ssh -t wz00 "echo yes" |    ssh wz07 hostname
-ssh -t wz00 "echo yes" |    ssh wz08 hostname
+ssh -t wz00 "echo yes" &&    ssh wz01 hostname
+ssh -t wz00 "echo yes" &&    ssh wz02 hostname
+ssh -t wz00 "echo yes" &&    ssh wz03 hostname
+ssh -t wz00 "echo yes" &&    ssh wz04 hostname
+ssh -t wz00 "echo yes" &&    ssh wz05 hostname
+ssh -t wz00 "echo yes" &&    ssh wz06 hostname
+ssh -t wz00 "echo yes" &&    ssh wz07 hostname
+ssh -t wz00 "echo yes" &&    ssh wz08 hostname
